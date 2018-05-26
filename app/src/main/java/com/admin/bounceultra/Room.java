7fa0c82f0d7c9c2acb614e6aa5c17f16dcaf3291
Anton Paramonov <anton-paramonov2000@yandex.ru>
2018-05-26 21:54:41 +0300

    Рисует GameObjects в цикле. Мячик и прямоугольники

--- app/src/main/java/com/admin/bounceultra/Room.java
+++ app/src/main/java/com/admin/bounceultra/Room.java
@@ -8,5 +8,11 @@ public class Room {
         Point start_point = new Point(100, 100);
         Ball ball = new Ball(start_point, 50);
         ObjectList.add(ball);
+        Obstacle obst = new Obstacle();
+        obst.x_left = 100;
+        obst.y_top = 100;
+        obst.x_left = 300;
+        obst.y_bottom = 300;
+        ObjectList.add(obst);
     }
 }
