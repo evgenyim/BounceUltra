9a17004c92514c10c3c0277e3cc2ca69f919c07d
Григорий Эмдин <egd3700@mail.ru>
2018-07-21 17:07:50 +0300

    нормальный отскок от точки и хороший if

--- app/src/main/java/com/admin/bounceultra/Ball.java
+++ app/src/main/java/com/admin/bounceultra/Ball.java
@@ -147,14 +147,14 @@ class Ball extends GameObject implements Cloneable{
         float d;
         float new_x;
         float new_y;
+        Point E;
+        if (centre().dist(segment.a) < centre().dist(segment.b)) {
+            E = segment.a;
+        } else{
+            E = segment.b;
+        }
         if ((A == null)) {
             Vector normal = Segment.normal(segment);
-            Point E;
-            if (centre().dist(segment.a) < centre().dist(segment.b)) {
-                E = segment.a;
-            } else{
-                E = segment.b;
-            }
             Line p = new Line(E, normal);
             Point B = Line.intersect(n, p);
             l = (float) sqrt((x - B.x) * (x - B.x) + (y - B.y) * (y - B.y));
@@ -162,16 +162,11 @@ class Ball extends GameObject implements Cloneable{
             new_x = x + (B.x - x) * (l - d) / l;
             new_y = y + (B.y - y) * (l - d) / l;
         } else {
-            if ((min_d == centre().dist(segment.a)) || (min_d == centre().dist(segment.b))) {
-                Point p;
-                if (min_d == centre().dist(segment.a)) {
-                    p = segment.a;
-                } else {
-                    p = segment.b;
-                }
-                Line normal = new Line(n.b / n.a, -1, p.y - n.b * p.x / n.a);
+            Ball new_ball = new Ball(E.x, E.y, r);
+            if (bias.intersect_ball(new_ball)) {
+                Line normal = new Line(n.b / n.a, -1, E.y - n.b * E.x / n.a);
                 Point inters = Line.intersect(normal, n);
-                float OI = (float) sqrt(r * r - (p.x - inters.x) * (p.x - inters.x) - (p.y - inters.y) * (p.y - inters.y));
+                float OI = (float) sqrt(r * r - (E.x - inters.x) * (E.x - inters.x) - (E.y - inters.y) * (E.y - inters.y));
                 Point Q = new Point(-n.c / n.a,0);
                 Vector vect_OI = new Vector(inters, Q);
                 vect_OI.unit();
--- app/src/main/java/com/admin/bounceultra/SimpleSegment.java
+++ app/src/main/java/com/admin/bounceultra/SimpleSegment.java
@@ -9,16 +9,13 @@ class SimpleSegment extends Segment {
     void comunicate(Ball ball, int intersected_seg_ind, Segment cur_seg, ArrayList<GameObject> ObjectList, int intersected_obj_ind, float min_d, boolean draft) {
         ArrayList<Segment> segments = ObjectList.get(intersected_obj_ind).segments;
         if ((!ball.stick_to_segment(cur_seg)) ||  (min_d  == ball.centre().dist(segments.get(intersected_seg_ind).a) || min_d  == ball.centre().dist(segments.get(intersected_seg_ind).b))) {
-            Log.d("antihui", "antiihui");
             float axil = 1;
             ball.MindTheGap(segments.get(intersected_seg_ind), axil, min_d);
             axil *= ball.g;
             min_d = Vector.dPointSegment(segments.get(intersected_seg_ind).a, segments.get(intersected_seg_ind).b, ball.centre());
             Vector segment = new Vector(segments.get(intersected_seg_ind));
             if (min_d  == ball.centre().dist(segments.get(intersected_seg_ind).a) || min_d  == ball.centre().dist(segments.get(intersected_seg_ind).b)) {
-                Log.d("3","3");
-                Vector new_velocity;
-                Point p = new Point(0,0);
+                Point p;
                 if (min_d  == ball.centre().dist(segments.get(intersected_seg_ind).a)) {
                     p = segments.get(intersected_seg_ind).a;
                 } else {
@@ -27,15 +24,12 @@ class SimpleSegment extends Segment {
                 float a = p.x - ball.centre().x;
                 float b = p.y - ball.centre().y;
                 float c = 12;
-                //float c = - ball.centre().x * p.x - ball.centre().y * p.y + ball.centre().x * ball.centre().x + ball.centre().y * ball.centre().y - ball.r * ball.r;
                 Segment seg = new Segment(a, b, c);
                 Vector vec = new Vector(seg);
                 ball.velocity = Vector.reflect(ball.velocity, vec);
 
             } else {
-                Log.d("1","1");
                 if (!ball.velocity.if_null()) {
-                    Log.d("2","2");
                     ball.velocity = Vector.reflect(ball.velocity, segment);
                 }
             }
@@ -43,7 +37,6 @@ class SimpleSegment extends Segment {
             ball.x_speed = ball.velocity.x * (1 - ball.decreas);
             ball.y_speed = ball.velocity.y * (1 - ball.decreas);
         } else {
-            Log.d("hui", "hui:");
             ball.roll(cur_seg);
             ball.x_speed = ball.velocity.x;
             ball.y_speed = ball.velocity.y;
